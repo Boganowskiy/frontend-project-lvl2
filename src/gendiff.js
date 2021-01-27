69c6ae6a881f07edc968704897688b98574429aa
Boganowskiy Andrey <boganowskiy@gmail.com>
2021-01-27 18:57:34 +0300

    added option --format

--- src/gendiff.js
+++ src/gendiff.js
@@ -2,7 +2,10 @@ import { Command } from 'commander';
 
 const program = new Command();
 
-program.version('0.1');
-program.description('Compares two configuration files and shows a difference.');
+program
+  .version('0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .usage('[options] <filepath1> <filepath2>');
 
 export default program;
