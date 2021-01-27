7028a79d8d9d69b142960c045668ff6314a3f000
Boganowskiy Andrey <boganowskiy@gmail.com>
2021-01-27 20:42:50 +0300

    added getObjectDiff func

---
+++ src/getObjectDiff.js
@@ -0,0 +1,27 @@
+import _ from 'lodash';
+
+const getObjectsDiff = (obj1, obj2) => {
+  const obj1Entries = Object.entries(obj1);
+  const resultWithObj1Elements = obj1Entries.reduce((acc, [key, value]) => {
+    const oldValue = `- ${key}: ${value}`;
+    if (_.has(obj2, key)) {
+      if (obj2[key] === value) {
+        const newElem = `  ${key}: ${value}`;
+        return [...acc, newElem];
+      }
+      const newValue = `+ ${key}: ${obj2[key]}`;
+      return [...acc, oldValue, newValue];
+    }
+    return [...acc, oldValue];
+  }, []);
+  const obj1Keys = Object.keys(obj1);
+  const obj2WithUniqueKeysElements = _.omit(obj2, obj1Keys);
+  const obj2WithUniqueKeysElementsEntries = Object.entries(obj2WithUniqueKeysElements);
+  const resultArr = obj2WithUniqueKeysElementsEntries.reduce((acc, [key, value]) => (
+    [...acc, `+ ${key}: ${value}`]
+  ), resultWithObj1Elements);
+  const resultStr = `{\n\t${resultArr.join('\n\t')}\n}`;
+  return resultStr;
+};
+
+export default getObjectsDiff;
