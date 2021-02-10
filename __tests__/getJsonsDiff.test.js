3ab30717c51b7f2ac0d2e5f8d7d831df238c5fe7
Boganowskiy Andrey <boganowskiy@gmail.com>
2021-02-10 21:27:24 +0300

    added test files and fixtures

---
+++ __tests__/getJsonsDiff.test.js
@@ -0,0 +1,20 @@
+import { test, expect } from '@jest/globals';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+import getJsonsDiff from '../src/getJsonsDiff.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => resolve(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+test('check 1', () => {
+  const filepath1 = getFixturePath('file1.json');
+  const filepath2 = getFixturePath('file2.json');
+  const expectedFilepath = getFixturePath('result1.txt');
+  const expectedResultContent = readFile(expectedFilepath);
+  const result = getJsonsDiff(filepath1, filepath2);
+  expect(result).toBe(expectedResultContent);
+});
