25740b0402d389ba9b32bdb06e371ecb5cf39492
Boganowskiy Andrey <boganowskiy@gmail.com>
2021-03-02 13:34:07 +0300

    added test for .yml files

--- __tests__/getJsonsDiff.test.js
+++ __tests__/getJsonsDiff.test.js
@@ -10,9 +10,18 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => resolve(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('check 1', () => {
+test('check jsons files', () => {
   const filepath1 = getFixturePath('file1.json');
   const filepath2 = getFixturePath('file2.json');
+  const expectedFilepath = getFixturePath('result1.txt');
+  const expectedResultContent = readFile(expectedFilepath);
+  const result = getJsonsDiff(filepath1, filepath2);
+  expect(result).toBe(expectedResultContent);
+});
+
+test('check yaml files', () => {
+  const filepath1 = getFixturePath('file3.yml');
+  const filepath2 = getFixturePath('file4.yml');
   const expectedFilepath = getFixturePath('result1.txt');
   const expectedResultContent = readFile(expectedFilepath);
   const result = getJsonsDiff(filepath1, filepath2);
