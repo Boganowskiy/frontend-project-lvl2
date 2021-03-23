4b0dcbcd2f0d589182abba2c9189699a87864365
Boganowskiy Andrey <boganowskiy@gmail.com>
2021-03-23 14:22:04 +0300

    reduced Cognitive Complexity in getObjectDiff, removed some similar some similars blocks of code

--- src/getObjectDiff.js
+++ src/getObjectDiff.js
@@ -1,68 +1,53 @@
 import _ from 'lodash';
 
 const getObjectsDiff = (obj1, obj2) => {
-  const iter = (o1, o2) => {
+  const buildAst = (o1, o2) => {
     const keys1 = Object.keys(o1);
     const keys2 = Object.keys(o2);
     const keys = _.union(keys1, keys2).sort();
     const result = keys.reduce((acc, key) => {
+      const oldValue = o1[key];
+      const newValue = o2[key];
+      const status = 'unchanged';
       const node = {
-        key, status: 'unchanged', oldValue: null, newValue: null,
+        key, status, oldValue, newValue,
       };
-      const value1 = o1[key];
-      const value2 = o2[key];
-      if (Array.isArray(value1) && Array.isArray(value2)) {
-        if (_.isEqual(value1, value2)) {
-          return [...acc, { ...node, oldValue: value1, newValue: value2 }];
+      if (Array.isArray(oldValue) && Array.isArray(newValue)) {
+        const newStatus = (_.isEqual(oldValue, newValue)) ? status : 'changed';
+        return [...acc, { ...node, status: newStatus }];
+      }
+      if (!(_.has(o1, key)) || !(_.has(o2, key))) {
+        const data = (!(_.has(o1, key)))
+          ? { newStatus: 'added', value: newValue }
+          : { newStatus: 'deleted', value: oldValue };
+        const { value, newStatus } = data;
+        const newNodeData = { status: newStatus };
+        if (value instanceof Object && !Array.isArray(value)) {
+          newNodeData.children = buildAst(value, value);
         }
+        return [...acc, { ...node, ...newNodeData }];
+      }
+      if (oldValue instanceof Object || newValue instanceof Object) {
+        if (oldValue instanceof Object && newValue instanceof Object) {
+          return [...acc, { ...node, children: buildAst(oldValue, newValue) }];
+        }
+        const obj = (oldValue instanceof Object)
+          ? oldValue
+          : newValue;
         return [...acc, {
-          ...node, status: 'changed', oldValue: value1, newValue: value2,
+          ...node, status: 'changed', children: buildAst(obj, obj),
         }];
       }
-      if (!_.has(o1, key) && _.has(o2, key)) {
-        if (Array.isArray(value2)) {
-          return [...acc, { ...node, status: 'added', newValue: value2 }];
-        }
-        if (value2 instanceof Object) {
-          return [...acc, { ...node, status: 'added', children: iter(value2, value2) }];
-        }
-        return [...acc, { ...node, status: 'added', newValue: value2 }];
-      }
-      if (_.has(o1, key) && !_.has(o2, key)) {
-        if (Array.isArray(value1)) {
-          return [...acc, { ...node, status: 'deleted', oldValue: value1 }];
-        }
-        if (value1 instanceof Object) {
-          return [...acc, { ...node, status: 'deleted', children: iter(value1, value1) }];
-        }
-        return [...acc, { ...node, status: 'deleted', oldValue: value1 }];
-      }
-
-      if (value1 instanceof Object && value2 instanceof Object) {
-        return [...acc, { ...node, children: iter(value1, value2) }];
-      }
-      if (value1 instanceof Object) {
-        return [...acc, {
-          ...node, status: 'changed', children: iter(value1, value1), newValue: value2,
-        }];
-      }
-      if (value2 instanceof Object) {
-        return [...acc, {
-          ...node, status: 'changed', oldValue: value1, children: iter(value2, value2),
-        }];
-      }
-      if (value1 === value2) {
-        return [...acc, {
-          ...node, status: 'unchanged', oldValue: value1, newValue: value2,
-        }];
+      if (oldValue === newValue) {
+        return [...acc, node];
       }
       return [...acc, {
-        ...node, status: 'changed', oldValue: value1, newValue: value2,
+        ...node, status: 'changed', oldValue, newValue,
       }];
     }, []);
     return result;
   };
-  return iter(obj1, obj2);
+  return buildAst(obj1, obj2);
 };
 
 export default getObjectsDiff;
--- src/stylish.js
+++ src/stylish.js
@@ -4,32 +4,26 @@ const prefixes = {
   unchanged: '    ',
 };
 
-const objStyle = {
-  added: (ind, key, _oldV, _newV, children) => `
-${ind}${prefixes.added}${key}: {${children}
-${prefixes.unchanged}${ind}}`,
-  deleted: (ind, key, _oldV, _newV, children) => `
-${ind}${prefixes.deleted}${key}: {${children}
-${prefixes.unchanged}${ind}}`,
-  unchanged: (ind, key, _oldV, _newV, children) => `
-${ind}${prefixes.unchanged}${key}: {${children}
-${prefixes.unchanged}${ind}}`,
-
-  changed: (ind, key, oldV, newV, children) => {
-    if (oldV) {
+const formatObj = (ind, status, key, oldV, newV, children) => {
+  if (status === 'changed') {
+    if (oldV instanceof Object) {
       return `
-${ind}${prefixes.deleted}${key}: ${oldV}
-${ind}${prefixes.added}${key}: {${children}
-${prefixes.unchanged}${ind}}`;
-    }
-    return `
 ${ind}${prefixes.deleted}${key}: {${children}
 ${prefixes.unchanged}${ind}}
 ${ind}${prefixes.added}${key}: ${newV}`;
-  },
+    }
+    return `
+${ind}${prefixes.deleted}${key}: ${oldV}
+${ind}${prefixes.added}${key}: {${children}
+${prefixes.unchanged}${ind}}`;
+  }
+
+  return `
+${ind}${prefixes[status]}${key}: {${children}
+${prefixes.unchanged}${ind}}`;
 };
 
-const style = {
+const formatPrimitive = {
   added: (ind, key, _oldV, newV) => `
 ${ind}${prefixes.added}${key}: ${newV}`,
 
@@ -50,17 +44,13 @@ const stylish = (ast) => {
       key, status, oldValue, newValue, children,
     } = node;
     const currentIndent = prefixes.unchanged.repeat(depth);
-    if (!children) {
-      const oldV = (oldValue && Array.isArray(oldValue)) ? `[ ${oldValue} ]` : oldValue;
-      const newV = (newValue && Array.isArray(newValue)) ? `[ ${newValue} ]` : newValue;
-      return style[status](currentIndent, key, oldV, newV);
-    }
-
     if (children) {
-      return objStyle[status](currentIndent, key, oldValue, newValue, children.flatMap((child) => (
-        iter(child, depth + 1))).join(''));
+      const childrenArr = children.flatMap((child) => iter(child, depth + 1)).join('');
+      return formatObj(currentIndent, status, key, oldValue, newValue, childrenArr);
     }
-    return style[status](currentIndent, key, oldValue, newValue);
+    const oldV = (oldValue && Array.isArray(oldValue)) ? `[ ${oldValue} ]` : oldValue;
+    const newV = (newValue && Array.isArray(newValue)) ? `[ ${newValue} ]` : newValue;
+    return formatPrimitive[status](currentIndent, key, oldV, newV);
   };
   const resArr = ast.map((child) => iter(child, 0));
   return `{${resArr.join('')}
